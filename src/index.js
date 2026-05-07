c02f94b8458cd6caa20dcaa87648ce96b0af7acc
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-05-07 07:47:38 +0700

    Convert functional structure to class-based architecture

--- src/index.js
+++ src/index.js
@@ -1,59 +1,77 @@
-let weatherAPIKey = 'LNVCDZJPD9GVSBRL4W4DENXNH';
-let airQualityAPIKey = '0bcba15c13c8d2ef4a5cf53d5af7669cdcb5a366';
-
-async function takeWeatherConditionOfCity(city, APIKey) {
-    const response = await fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${APIKey}&unitGroup=metric`,
-    );
-    if (!response.ok) {
-        throw Error(`HTTP ERROR! Status: ${response.status}`);
+class FetchData {
+    constructor() {
+        this.weatherAPIKey = 'LNVCDZJPD9GVSBRL4W4DENXNH';
+        this.airQualityAPIKey = '0bcba15c13c8d2ef4a5cf53d5af7669cdcb5a366';
     }
-    const json = await response.json();
-    return json;
-}
 
-async function takeAirQualityOfCity(city, APIKey) {
-    const response = await fetch(
-        `https://api.waqi.info/feed/${city}/?token=${APIKey}`,
-    );
-    if (!response.ok) {
-        throw Error(`HTTP ERROR! status: ${response.status}`);
+    async takeWeatherConditionOfCity(city) {
+        const response = await fetch(
+            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${this.weatherAPIKey}&unitGroup=metric`,
+        );
+        if (!response.ok) {
+            throw Error(`HTTP ERROR! Status: ${response.status}`);
+        }
+        const json = await response.json();
+        return json;
     }
-    const json = await response.json();
-    console.log(json);
-    if (json.status === 'error') {
-        throw Error(`Not valid city`);
-    }
-    return json;
-}
 
-async function setContent(city, weatherAPIKey, airQualityAPIKey) {
-    try {
-        const [weather, airQuality] = await Promise.all([
-            takeWeatherConditionOfCity(city, weatherAPIKey),
-            takeAirQualityOfCity(city, airQualityAPIKey),
-        ]);
-        document.getElementById('city').textContent = weather.address;
-        document.getElementById('temp').textContent =
-            Math.round(weather.currentConditions.temp) + '°C';
-        document.getElementById('AQI').textContent =
-            airQuality.data.aqi + 'AQI';
-    } catch (error) {
-        console.error(error);
-        alert(error);
+    async takeAirQualityOfCity(city) {
+        const response = await fetch(
+            `https://api.waqi.info/feed/${city}/?token=${this.airQualityAPIKey}`,
+        );
+        if (!response.ok) {
+            throw Error(`HTTP ERROR! status: ${response.status}`);
+        }
+        const json = await response.json();
+        console.log(json);
+        if (json.status === 'error') {
+            throw Error(`Not valid city`);
+        }
+        return json;
     }
 }
 
-const searchBtn = document.getElementById('search_btn');
-searchBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const city = document.getElementById('input_city').value;
-    setContent(city, weatherAPIKey, airQualityAPIKey);
-});
+class RenderData {
+    constructor() {
+        this.fetchData = new FetchData();
 
-document.getElementById('input_city').addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-        const city = document.getElementById('input_city').value;
-        setContent(city, weatherAPIKey, airQualityAPIKey);
+        this.searchBtn = document.getElementById('search_btn');
+        this.inputCity = document.getElementById('input_city');
+
+        this.bindEvent();
     }
-});
+    bindEvent() {
+        this.searchBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            const city = this.inputCity.value;
+            this.setContent(city);
+        });
+        this.inputCity.addEventListener('keydown', (e) => {
+            if (e.key === 'Enter') {
+                const city = this.inputCity.value;
+                this.setContent(city);
+            }
+        });
+    }
+
+    async setContent(city) {
+        try {
+            const [weather, airQuality] = await Promise.all([
+                this.fetchData.takeWeatherConditionOfCity(city),
+                this.fetchData.takeAirQualityOfCity(city),
+            ]);
+            document.getElementById('city').textContent = weather.address;
+            document.getElementById('temp').textContent =
+                Math.round(weather.currentConditions.temp) + '°C';
+            document.getElementById('AQI').textContent =
+                airQuality.data.aqi + 'AQI';
+        } catch (error) {
+            console.error(error);
+            alert(error);
+
+            // handle error here
+        }
+    }
+}
+
+new RenderData();
